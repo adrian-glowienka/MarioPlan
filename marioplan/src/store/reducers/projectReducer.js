5e6227971752637da32af2b059b1165686baad37
adrian-glowienka <45940806+adrian-glowienka@users.noreply.github.com>
2019-10-02 11:04:57 +0200

    connected firebase firestore to app

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -9,8 +9,12 @@ const initState = {
 const projectReducer = ( state= initState, action ) => {
     switch(action.type) {
         case 'CREATE_PROJECT':
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            return state;
+        default:
+            return state;
     }
-    return state
 }
 
 export default projectReducer;
